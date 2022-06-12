8c0a42992b276eaf0a6b96eae3be44623e80fdb1
pritam-patro <patropritam@gmail.com>
2022-06-12 17:35:18 +0530

    second commit

---
+++ like.js
@@ -0,0 +1,7 @@
+const likeButtons = document.querySelectorAll(".like-btn");
+function handleLike(event){
+    event.target.classList.toggle("active");
+}
+likeButtons.forEach((likeButtons) => {
+    likeButtons.addEventListener("click",handleLike);
+});
